2d9990bf48be611bf90654c5b2f1111f8a2e68b1
Adrian Konnek <konnek.adrian@gmail.com>
2022-06-30 17:50:51 +0200

    full functionality of summaring ingredients and bases

--- src/components/compose.js
+++ src/components/compose.js
@@ -12,76 +12,82 @@ function Compose() {
     });
     setIngredients(Ingredients);
   }, []);
-
-  const ingredientChange = (ingredient) => {
-    setIngredients(
-      ingredients.map((el) => {
-        if (el.name === ingredient.name) el.checked = !el.checked;
-
-        return el;
-      })
-    );
-
-    setPrice(
-      ingredients.reduce((summary, ingredient) => {
-        return ingredient.checked ? summary + ingredient.cost : summary;
-      }, base)
-    );
-  };
-  return (
-    <div className="compose">
-      <h1>Compose pizza</h1>
-      <h4>Price: {(price / 100).toFixed(2)}$</h4>
-      <div>
-        <img
-          className={"size small"}
-          src={process.env.PUBLIC_URL + "/assets/size.png"}
-          alt="small size"
-        />
-        <img
-          className={"size medium"}
-          src={process.env.PUBLIC_URL + "/assets/size.png"}
-          alt="medium size"
-        />
-        <img
-          className={"size big"}
-          src={process.env.PUBLIC_URL + "/assets/size.png"}
-          alt="big size"
-        />
-      </div>
-      <div>
-        <button className="btn">Add</button>
-      </div>
-      <div className="ingredients">
-        {ingredients.map((ingredient, index) => {
-          return (
-            <div key={index} className="ingredientRow">
-              <input
-                type="checkbox"
-                checked={ingredient.checked}
-                onChange={() => ingredientChange(ingredient)}
-              />
-              <img
-                className="ingredient_icon"
-                src={
-                  process.env.PUBLIC_URL + "/assets/" + ingredient.name + ".png"
-                }
-                alt={ingredient.name}
-              />
-              <p>{ingredient.name}</p>
-              {ingredient.cost === 0 ? (
-                <p class="ingredient_price">free</p>
-              ) : (
-                <p class="ingredient_price">
-                  {(ingredient.cost / 100).toFixed(2)}$
-                </p>
-              )}
-            </div>
-          );
-        })}
-      </div>
-    </div>
+useEffect(() => {
+  setPrice(
+    ingredients.reduce((summary, ingredient) => {
+      return ingredient.checked ? summary + ingredient.cost : summary;
+    }, base)
   );
+}, [base, ingredients]);
+const ingredientChange = (ingredient) => {
+  setIngredients(
+    ingredients.map((el) => {
+      if (el.name === ingredient.name) el.checked = !el.checked;
+
+      return el;
+    })
+  );
+};
+const changeSize = (size) => {
+  setBase(size);
+};
+return (
+  <div className="compose">
+    <h1>Compose pizza</h1>
+    <h4>Price: {(price / 100).toFixed(2)}$</h4>
+    <div>
+      <img
+        className={"size small " + (base === 500 ? "checked" : "")}
+        onClick={() => changeSize(500)}
+        src={process.env.PUBLIC_URL + "/assets/size.png"}
+        alt="small size"
+      />
+      <img
+        className={"size medium " + (base === 700 ? "checked" : "")}
+        onClick={() => changeSize(700)}
+        src={process.env.PUBLIC_URL + "/assets/size.png"}
+        alt="medium size"
+      />
+      <img
+        className={"size big " + (base === 1000 ? "checked" : "")}
+        onClick={() => changeSize(1000)}
+        src={process.env.PUBLIC_URL + "/assets/size.png"}
+        alt="big size"
+      />
+    </div>
+    <div>
+      <button className="btn">Add</button>
+    </div>
+    <div className="ingredients">
+      {ingredients.map((ingredient, index) => {
+        return (
+          <div key={index} className="ingredientRow">
+            <input
+              type="checkbox"
+              checked={ingredient.checked}
+              onChange={() => ingredientChange(ingredient)}
+            />
+            <img
+              className="ingredient_icon"
+              src={
+                process.env.PUBLIC_URL + "/assets/" + ingredient.name + ".png"
+              }
+              alt={ingredient.name}
+            />
+            <p>{ingredient.name}</p>
+            {ingredient.cost === 0 ? (
+              <p class="ingredient_price">free</p>
+            ) : (
+              <p class="ingredient_price">
+                {(ingredient.cost / 100).toFixed(2)}$
+              </p>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  </div>
+);
 }
 
 export default Compose;
